606de5776cdaa0a2ef9f80bd122f157dabd2747e
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-11 14:27:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Your_name_and_age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Mokgadi";
+            string lastName = "Thobejane";
+            string name = $"My Name is: {firstName} {lastName}";
+
+            
+           Console.WriteLine(name);
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is:" + age);
+        }
+    }
+}
